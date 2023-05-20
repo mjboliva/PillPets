be8956518297920fa9f9bf460399995dc6433e2b
mjboliva <madisonjulianna.oliva@gmail.com>
2023-05-20 14:13:13 -0700

    Initial Push of First Project Files
    Just the default project files when you make the project.

---
+++ PillPets/PillPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillPetsApp.swift
+//  PillPets
+//
+//  Created by Madison Juliana Oliva on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PillPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
