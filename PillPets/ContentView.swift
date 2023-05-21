9ec18367f361b3cbcf78fc7f0cd59f26b9cd4a97
Prathmesh Jain <prathmeshjain02@gmail.com>
2023-05-20 20:01:12 -0700

    Setup Bottom Sheets

--- PillPets/ContentView.swift
+++ PillPets/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isFaqVisible = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Open FAQ"){
+                isFaqVisible = true
+            }
         }
         .padding()
+        .sheet(isPresented: $isFaqVisible){
+            Faq()
+        }
     }
 }
 
---
+++ PillPets/Faq.swift
@@ -0,0 +1,23 @@
+//
+//  Faq.swift
+//  PillPets
+//
+//  Created by Prathmesh Jain on 5/20/23.
+//
+
+import SwiftUI
+
+struct Faq: View {
+    var body: some View {
+        VStack{
+            Text("Question?")
+            Text("Answer")
+        }
+    }
+}
+
+struct Faq_Previews: PreviewProvider {
+    static var previews: some View {
+        Faq()
+    }
+}
